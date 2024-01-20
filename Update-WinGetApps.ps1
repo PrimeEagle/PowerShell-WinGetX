dd708ccb9a89ef2de16fe4e0a3f66f4416d40f5f
PrimeEagle <misc@hiddendragon.com>
2024-01-19 23:49:23 -0600

    update template code

--- Update-WinGetApps.ps1
+++ Update-WinGetApps.ps1
@@ -24,11 +24,13 @@ DynamicParam { Build-BaseParameters }
 Begin
 {	
 	Write-LogTrace "Execute: $(Get-RootScriptName)"
+	$minParams = Get-MinimumRequiredParameterCount -CommandInfo (Get-Command $MyInvocation.MyCommand.Name)
 	$cmd = @{}
 
 	if(Get-BaseParamHelpFull) { $cmd.HelpFull = $true }
-	if((Get-BaseParamHelpDetail) -Or ($PSBoundParameters.Count -eq 0)) { $cmd.HelpDetail = $true }
+	if((Get-BaseParamHelpDetail) -Or ($PSBoundParameters.Count -lt $minParams)) { $cmd.HelpDetail = $true }
 	if(Get-BaseParamHelpSynopsis) { $cmd.HelpSynopsis = $true }
+	
 	if($cmd.Count -gt 1) { Write-DisplayHelp -Name "$(Get-RootScriptPath)" -HelpDetail }
 	if($cmd.Count -eq 1) { Write-DisplayHelp -Name "$(Get-RootScriptPath)" @cmd }
 }
@@ -37,12 +39,7 @@ Process
 	try
 	{
 		$isDebug = Assert-Debug
-		
-		if(-Not (Assert-PathQueueParameter))
-		{
-			Write-DisplayHelp -Name "$(Get-RootScriptPath)" -HelpDetail
-		}
-	
+			
 		$exclusionPatterns = @()
 		
 		if($ExclusionFile)
